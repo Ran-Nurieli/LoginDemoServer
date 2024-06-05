a17c83cc4a2f2d2d8c80931542108c0b5a75f859
User <User@LAB3-3>
2024-06-05 10:12:43 +0300

    aa

--- LoginDemoServer/DBScripts/Create.sql
+++ LoginDemoServer/DBScripts/Create.sql
@@ -13,19 +13,19 @@ Create Table Users (
 Go
 
 Create Table Grades (
-Email nvarchar(100),
+[Email] nvarchar(100) NOT NULL,
 FOREIGN KEY (Email) REFERENCES Users(Email),
 [Subject] nvarchar(100) NOT NULL,
-BirthDate DATETIME NULL,
-[Date] int NULL,
-Grade int Null
+[Date] DATETIME NOT NULL,
+Grade int NOT NULL
 
 )
 Go
 
 INSERT INTO dbo.Users VALUES ('talsi@talsi.com', '1234', '+972506665835','20-may-1976',1,'Tal Simon')
 INSERT INTO dbo.Grades VALUES ('talsi@talsi.com', 'Math','20-may-1976','95')
-INSERT INTO dbo.Grades VALUES ('ran123.com', 'English','14-January-2007','95')
+INSERT INTO dbo.Grades VALUES ('ran123@gmail.com', 'English','3-January-2004','86')
+INSERT INTO dbo.Grades VALUES ('ran123.com', 'Music','16-March-2007','85')
 Go
 
 --scaffold-DbContext "Server = (localdb)\MSSQLLocalDB;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
