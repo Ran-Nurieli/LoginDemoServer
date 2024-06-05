b23358ceb39b613ac3bc52925990ac8390a8ae39
Nyx <Nyx@DESKTOP-RIC45Q6>
2024-06-05 07:31:28 +0300

    aa

--- LoginDemoServer/DBScripts/Create.sql
+++ LoginDemoServer/DBScripts/Create.sql
@@ -12,7 +12,20 @@ Create Table Users (
 )
 Go
 
+Create Table Grades (
+Email nvarchar(100),
+FOREIGN KEY (Email) REFERENCES Users(Email),
+[Subject] nvarchar(100) NOT NULL,
+BirthDate DATETIME NULL,
+[Date] int NULL,
+Grade int Null
+
+)
+Go
+
 INSERT INTO dbo.Users VALUES ('talsi@talsi.com', '1234', '+972506665835','20-may-1976',1,'Tal Simon')
+INSERT INTO dbo.Grades VALUES ('talsi@talsi.com', 'Math','20-may-1976','95')
+INSERT INTO dbo.Grades VALUES ('ran123.com', 'English','14-January-2007','95')
 Go
 
 --scaffold-DbContext "Server = (localdb)\MSSQLLocalDB;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
